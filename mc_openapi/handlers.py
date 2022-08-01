254e8ffa481b68061a321fba8e5358c1b4cdc5e4
Michele Chiari <michele.chiari@polimi.it>
2022-08-01 18:22:28 +0200

    Revert "Disable multithreading."
    This reverts commit bf2261f64d91f67f9e9cd119ef1dde4a77ccf367.

--- mc_openapi/handlers.py
+++ mc_openapi/handlers.py
@@ -13,7 +13,7 @@ def post(body, requirement=None):
     doml_xmi = body
     try:
         dmc = ModelChecker(doml_xmi)
-        results = dmc.check_common_requirements(threads=1, consistency_checks=False)
+        results = dmc.check_common_requirements(2)
         res, msg = results.summarize()
 
         if res == MCResult.sat:
