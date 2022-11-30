841414bed1ea8322655ed750686ea8f57adf7794
Andrea Franchini <hello@andreafranchini.com>
2022-11-30 11:46:22 +0100

    add CI for docs

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ templates_path = []
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'requirements.txt']
 
 
 # -- Options for HTML output -------------------------------------------------
